69badf2e6a2fe9d60548901bb488459a8c75585d
ClaudeFalzon <cjfalzon@gmail.com>
2021-11-16 16:22:47 +0100

    router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 import React from "react"
 import App from "./App.js"
 import reactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom";
 
-reactDOM.render( <BrowserRouter><App/></BrowserRouter> ,document.getElementById("root"))
+reactDOM.render( <HashRouter><App/></HashRouter> ,document.getElementById("root"))
