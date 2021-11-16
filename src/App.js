d2f42d83e8a2a7296cebe426d3c1347abee6275b
ClaudeFalzon <cjfalzon@gmail.com>
2021-11-16 15:50:01 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes, useNavigate } from "react-router";
 import "./App.css";
 import About from "./components/About";
